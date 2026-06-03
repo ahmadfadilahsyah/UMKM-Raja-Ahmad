7b3e5c7897f6e3b2fe1a56353e06517b7c2607e3
ahmadfadilahsyah <ahmadfadilahsyah@gmail.com>
2026-06-03 20:16:36 +0700

    Add files via upload

---
+++ umkm_raja_ahmad.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Jun 2026 pada 19.33
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `umkm_raja_ahmad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi_masuk`
+--
+-- Kesalahan membaca struktur untuk tabel umkm_raja_ahmad.detail_transaksi_masuk: #1932 - Table &#039;umkm_raja_ahmad.detail_transaksi_masuk&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel umkm_raja_ahmad.detail_transaksi_masuk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `umkm_raja_ahmad`.`detail_transaksi_masuk`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+-- Kesalahan membaca struktur untuk tabel umkm_raja_ahmad.produk: #1932 - Table &#039;umkm_raja_ahmad.produk&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel umkm_raja_ahmad.produk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `umkm_raja_ahmad`.`produk`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `staff`
+--
+-- Kesalahan membaca struktur untuk tabel umkm_raja_ahmad.staff: #1932 - Table &#039;umkm_raja_ahmad.staff&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel umkm_raja_ahmad.staff: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `umkm_raja_ahmad`.`staff`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi_masuk`
+--
+-- Kesalahan membaca struktur untuk tabel umkm_raja_ahmad.transaksi_masuk: #1932 - Table &#039;umkm_raja_ahmad.transaksi_masuk&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel umkm_raja_ahmad.transaksi_masuk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `umkm_raja_ahmad`.`transaksi_masuk`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
